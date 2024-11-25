7f2c006efddb7913eab3314c3e79169d11589b89
shaquinbale <scopelenskingdra@gmail.com>
2024-11-25 12:22:58 -0500

    Add statement handler functions

--- lib/parse/parse.cr
+++ lib/parse/parse.cr
@@ -37,5 +37,59 @@ class Parser
 
   # program = {statement}
   def program
+    while check_token(TokenType::NEWLINE)
+      next_token
+    end
+
+    until @current_token == TokenType::EOF
+      statement
+    end
+  end
+
+  # statement = any of the keywords, really
+  def statement
+    # 'let', ident, '=', expression, ';'
+    if check_token(TokenType::LET)
+      handle_let
+    end
+
+    # 'while', comparison, '{', {statement}, '}', ';';
+    elsif check_token(TokenType::WHILE)
+      handle_while
+    end
+
+    # 'print', (expression | string), ';'
+    elsif check_token(TokenType::PRINT)
+      handle_print
+    end
+
+    # 'if', comparison, '{', {statement}, '}', ';'
+    elsif check_token(TokenType::IF)
+      handle_if
+    end
+
+    else
+      raise("Error: Expected valid keyword, got #{@current_token}")
+    end
+  end
+
+  # comparison = expression, (('>' | '>=' | '<' | '<=' | '==' | '!=') ,expression), {('&&' | '||'), comparison}
+  def comparison
+  end
+
+  # expression = term, {('+' | '-'), term}
+  def expression
+  end
+
+  # term = unary, {('*' | '/'), unary}
+  def term
+  end
+
+  # unary = ['+' | '-'], primary
+  def unary
+  end
+
+  # primary = number | ident | '(', expression, ')'
+  def primary
   end
 end
--- lib/parse/statementhandlers.cr
+++ lib/parse/statementhandlers.cr
@@ -1,3 +1,47 @@
 module StatementHandlers
-    
+    def handle_let
+      next_token
+
+      match(TokenType::IDENT)
+      match(TokenType::EQ)
+
+      expression
+    end
+
+    def handle_while
+      next_token
+
+      comparison
+
+      match(TokenType::LBRACE)
+      until check_token(TokenType::RBRACE)
+        statement
+      end
+
+      match(TokenType::RBRACE)
+    end
+
+    def handle_print
+      next_token
+
+      if check_token(TokenType::STRING)
+        next_token
+      else
+        next_token
+      end
+
+    end
+
+    def handle_if
+      next_token
+
+      comparison
+      match(TokenType::LBRACE)
+
+      until check_token(TokenType::RBRACE)
+        statement
+      end
+
+      match(TokenType::RBRACE)
+    end
 end
